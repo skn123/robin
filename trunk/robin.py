62ae85e18432a8dc81dbc6f0d0545afe21e2af73
robin <robin@4dde7dca-db58-11dd-9f21-5bfbb98aebe5>
2005-09-26 20:02:57 +0000

    DEVELOP: minimal architecture detection without autoconf.

--- trunk/robin.py
+++ trunk/robin.py
@@ -1,17 +1,18 @@
-import imp, os.path, __builtin__
+import imp, string, os.path, __builtin__
 
 libdir = os.path.dirname(__file__)
-machine = os.uname()[0] #os.environ["MACHINE"]
 model = ["RELEASE", "DEBUG"][os.environ.has_key("ROBIN_DEBUG")]
-soext = "TODO: autoconf"
 
-if machine == "win32":
-	target = "lib/%s/%s/robin_pyfe%s"
-else:
-	target = "lib/%s/%s/librobin_pyfe%s"
+# Detect architecture
+try:
+	uname = os.uname()[0]
+except AttributeError:
+	uname = os.getenv("OS")
+arch = uname.translate(string.maketrans("-_","  ")).split()[0].lower()
+soext = { 'windows': ".dll", 'cygwin': ".dll", 'hp': ".sl" }.get(arch, ".so")
 
-target = target % (model, machine, soext)
-target = "librobin_pyfe.dll"
+target = "librobin_pyfe" + soext
+
 imp.load_dynamic("robin", os.path.join(libdir, target))
 __builtin__.double = double
 __builtin__.char = char
@@ -19,7 +20,7 @@ __builtin__.ulong = ulong
 __builtin__.uint = uint
 __builtin__.uchar = uchar
 
-ldinfo = { 'm': machine, 'so': soext, \
+ldinfo = { 'm': arch, 'so': soext, \
            'suffix': "", 'confdir': "." }
 
 def here(file):
