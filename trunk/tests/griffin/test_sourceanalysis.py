f302f441c8f0edbfbd4385dd86ec9e11c1e74237
oripel <oripel@4dde7dca-db58-11dd-9f21-5bfbb98aebe5>
2006-10-06 19:40:33 +0000

    Some more simple tests

--- trunk/tests/griffin/test_sourceanalysis.py
+++ trunk/tests/griffin/test_sourceanalysis.py
@@ -2,15 +2,31 @@ import unittest
 import sourceanalysis
 
 class AggregateTests(unittest.TestCase):
-    def test_addBase(self):
-        aggregate = sourceanalysis.Aggregate()
+    def setUp(self):
+        self.aggregate = sourceanalysis.Aggregate()
+
+    def tearDown(self):
+        del self.aggregate
+
+    def test_addBase_retval(self):
         new_base = sourceanalysis.Aggregate()
         visibility = 2
 
-        connection = aggregate.addBase(new_base, visibility)
+        connection = self.aggregate.addBase(new_base, visibility)
 
-        self.assertEqual(visibility, connection.getVisibility())
+        assert visibility == connection.getVisibility()
         assert connection.getBase() is new_base
+
+    def test_basesIterator_empty(self):
+        assert not self.aggregate.baseIterator().hasNext()
+
+    def test_basesIterator_several(self):
+        bases = [sourceanalysis.Aggregate() for i in xrange(5)]
+        for base in bases:
+            self.aggregate.addBase(base, 1)
+
+        for expected_base, connection in zip(bases,self.aggregate.baseIterator()):
+            assert connection.getBase() is expected_base
 
 
 def suite():
