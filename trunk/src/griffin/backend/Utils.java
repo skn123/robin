507c9fc774f883dbecc776fa4f4c560c6d05f84b
ask <ask@4dde7dca-db58-11dd-9f21-5bfbb98aebe5>
2007-11-20 11:01:38 +0000

    type hash function

--- trunk/src/griffin/backend/Utils.java
+++ trunk/src/griffin/backend/Utils.java
@@ -1960,4 +1960,8 @@ public class Utils {
 		};
 		
 	static public Map defaultInstanceMap = null;
+
+	public static String getTypeHash(Type type) {
+		return Integer.toHexString(type.getBaseType().getFullName().hashCode());
+	}
 }
